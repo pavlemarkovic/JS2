994d94e115cda86c7e599b510fa6e3f10bd72bd7
Pavle Marković <50014655+pavlemarkovic@users.noreply.github.com>
2023-01-09 10:11:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
-function validacija() {
-     let input = document.querySelector('#email');
-     let value = input.value;
+let allTotal = 0;
 
-     if (value.includes('@') && value.includes('.')) {
-
-          let pozicijaAt = value.indexOf('@');
-          let pozicijaTacka = value.indexOf('.');
-          let izmedjuAtTacka = value.substring(pozicijaAt + 1, pozicijaTacka);
-
+function addToCart(element) {
+  let mainEl = element.closest('.single-item');
+  let price = mainEl.querySelector('.price').innerText;
+  let name = mainEl.querySelector('h3').innerText;
+  let quantity = mainEl.querySelector('input').value;
+  let cartItems = document.querySelector('.cart-items');
 
 
-          if (izmedjuAtTacka.length > 0) {
+  if (parseInt(quantity) > 0) {
 
-               let preAt = value.substring(0, pozicijaAt);
+    price = price.substring(1);
+    price = parseInt(price);
 
-               if (preAt.length > 2) {
+    let total = price * parseInt(quantity);
+    allTotal += total;
+    cartItems.innerHTML += `<div class="cart-single-item">
+      <h3> ${name} </h3>
+      <p> $${price} x ${quantity} = $<span>${total}</span></p>
+      <button onclick="removeFromCart(this)" class="remove-item"> Ukloni</button>
+                          
+                   </div> `;
 
-                    let posleTacke = value.substring(pozicijaTacka + 1, value.length);
+    document.querySelector('.total').innerText = `Total: ${allTotal}`;
+    element.innerText = 'Dodato';
+    element.setAttribute('disabled', 'true')
+  } else {
+    alert('odaberi kolicinu');
+  }
 
-                    if (posleTacke.length > 1) {
 
-                         console.log('email je validan');
-                    } else {
-                         console.log('email nije validan!');
-                    }
-               } else {
-                    console.log('mail nije validan');
-               }
+}
 
-          } else {
-               console.log('mail nije validan');
-          }
+function removeFromCart(element) {
+  let mainEl = element.closest('.cart-single-item');
+  let price = mainEl.querySelector('p span').innerText;
+  let name = mainEl.querySelector('h3').innerText;
+  let vegetables = document.querySelectorAll('.single-item');
 
-     }
 
-     }
+
+  price = parseInt(price)
+  allTotal -= price;
+  document.querySelector('.total').innerText = `Total $${allTotal}`
+  mainEl.remove();
+
+  vegetables.forEach(function (vege) {
+    let itemName = vege.querySelector('.si-content h3').innerText;
+    if (itemName === name) {
+      vege.querySelector('.actions input'), value = 0;
+      vege.querySelector('.actions button').removeAttribute('disabled');
+      vege.querySelector('.actions button').innerText = 'Dodaj';
+    }
+  });
+}
+
